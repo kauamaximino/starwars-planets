6b6d5541c568ee6bd4fbb0442b0f94594bb00f17
Kauã Maximino <kaua_maximino@outlook.com>
2022-04-22 16:15:28 -0300

    requisito 1ok

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Table from './components/Table';
 function App() {
   return (
     <PlanetsProvider>
-      <span>Hello, App!</span>
       <Table />
     </PlanetsProvider>
   );
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import propTypes from 'prop-types';
-import PlanetsContext from './planetsContext';
+import PlanetsContext from './PlanetsContext';
 import fetchPlanets from '../services/planetsAPI';
 
 function PlanetsProvider({ children }) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    const result = fetchPlanets();
-    setData(result.resutls);
+    const resultsPlanets = async () => {
+      const response = await fetchPlanets();
+      setData(response.results);
+    };
+    resultsPlanets();
   }, []);
 
+  const context = { data };
+
   return (
-    <PlanetsContext.Provider value={ data }>
+    <PlanetsContext.Provider value={ context }>
       { children }
     </PlanetsContext.Provider>
   );
